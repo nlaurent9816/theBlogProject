d7d24a2a910721f01fbd715df1b7f7db4f33bf54
Nicolas Laurent <17908741+nlaurent9816@users.noreply.github.com>
2020-07-19 21:01:45 +0200

    Réajout d'un élément CSS disparu

--- build_and_push_site.sh
+++ build_and_push_site.sh
@@ -23,7 +23,7 @@ publish_site () {
     then
       echo "Des choses à commiter !"
       git add --all
-      git commit -m "Mise à jour du site" -m "Commit : $sha  \nDate : $(date -Iseconds)"
+      git commit -m "Mise à jour du site" -m "Commit : $sha    Date : $(date -Iseconds)"
       git push
     else
       echo "Rien à commiter !"
